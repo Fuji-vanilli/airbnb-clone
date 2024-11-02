e2ed09501def2ffa06fab96afdeba8979b3f4401
Fuji-vanilli <ijufillivan@gmail.com>
2024-11-02 10:37:24 +0300

    update

--- airbnb-clone-front/src/app/layout/navbar/navbar.component.ts
+++ airbnb-clone-front/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { ToolbarModule } from 'primeng/toolbar';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AvatarComponent } from './avatar/avatar.component';
+import { CategoryComponent } from './category/category.component';
+import {DialogService} from "primeng/dynamicdialog";
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarModule,
+    MenuModule,
+    FontAwesomeModule,
+    AvatarComponent,
+    CategoryComponent
+  ],
+  providers: [
+    DialogService
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  location= 'Anywhere';
+  guests= 'Add guests';
+  dates= 'Add week'
+
+  toastService= inject(ToastService);
+
+  currentMenuItems: MenuItem[] | undefined= [];
+
+  ngOnInit(): void {
+    this.fetchMenu();
+    this.toastService.sendMessage({severity: "info", summary: "welcome back to airbnb home page"});
+  }
+
+  fetchMenu() {
+    return [
+      {
+        label: "Sing up",
+        styleClass: "font-bold"
+      },
+      {
+        label: "Log in"
+      }
+    ]
+  }
 
 }
